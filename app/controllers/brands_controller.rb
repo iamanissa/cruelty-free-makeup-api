06c4c63d5444272b9c26517cc5f5c75baa475fda
SuperJones <superjones@users.noreply.github.com>
2018-01-03 23:01:10 -0500

    Update controllers

---
+++ app/controllers/brands_controller.rb
@@ -0,0 +1,43 @@
+class BrandsController < ApplicationController
+  before_action :set_brand, only: [:show, :update, :destroy]
+
+  # GET /brands
+  def index
+    @brands = Brand.all
+    json_response(@brands)
+  end
+
+  # POST /brands
+  def create
+    @brand = Brand.create!(brand_params)
+    json_response(@brand, :created)
+  end
+
+  # GET /brands/:id
+  def show
+    json_response(@brand)
+  end
+
+  # PUT /brands/:id
+  def update
+    @brand.update(brand_params)
+    head :no_content
+  end
+
+  # DELETE /brands/:id
+  def destroy
+    @brand.destroy
+    head :no_content
+  end
+
+  private
+
+  def brand_params
+    # whitelist params
+    params.permit(:title, :created_by)
+  end
+
+  def set_brand
+    @brand = Brand.find(params[:id])
+  end
+end

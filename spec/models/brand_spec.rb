507a04a070a3786be58b0d44170224f103392e87
SuperJones <superjones@users.noreply.github.com>
2018-01-04 08:29:16 -0500

    Add & organize tests

--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -1,10 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Brand, type: :model do
-  # Association test
-  it { should have_many(:products).dependent(:destroy) }
+  describe 'Schema' do
+    it {is_expected.to have_db_column :id}
+    it {is_expected.to have_db_column :name}
+    it {is_expected.to have_db_column :url}
+    it {is_expected.to have_db_column :price_category}
+    it {is_expected.to have_db_column :created_at}
 
-  # Validation tests
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:url) }
+    it {is_expected.to have_db_column :updated_at}
+    # it {is_expected.to have_db_column :}
+
+    it "is_expected.to have exactly 6 columns" do
+      expect(subject.class.column_names.count).to equal(6)
+    end
+  end
+
+  describe 'Relationships' do
+    it { should have_many(:products).dependent(:destroy) }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:url) }
+  end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,10 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  # Association test
-  it { should belong_to(:brand) }
+  describe 'Schema' do
+    it {is_expected.to have_db_column :id}
+    it {is_expected.to have_db_column :name}
+    it {is_expected.to have_db_column :type}
+    it {is_expected.to have_db_column :color_number}
+    it {is_expected.to have_db_column :color_name}
 
-  # Validation test
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:type) }
+    it {is_expected.to have_db_column :ingredients}
+    it {is_expected.to have_db_column :brand_id}
+    it {is_expected.to have_db_column :created_at}
+    it {is_expected.to have_db_column :updated_at}
+    # it {is_expected.to have_db_column :}
+
+    it "is_expected.to have exactly 9 columns" do
+      expect(subject.class.column_names.count).to equal(9)
+    end
+  end
+
+  describe 'Relationships' do
+    it { should belong_to(:brand) }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:type) }
+  end
 end

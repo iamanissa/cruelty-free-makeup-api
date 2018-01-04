b41596271d76c34a7f5090ac4a66c3bd9eb5b5fb
SuperJones <superjones@users.noreply.github.com>
2018-01-03 21:03:47 -0500

    Add models & past tests

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,2 +1,7 @@
 class Brand < ApplicationRecord
+  # model association
+  has_many :products, dependent: :destroy
+
+  # validations
+  validates_presence_of :name, :url
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
+  # model association
   belongs_to :brand
+
+  # validation
+  validates_presence_of :name, :type
 end

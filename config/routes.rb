ff7eb2002c1ffb12c09e436e2b2d3262ce1d7db7
Anissa <anissajones10@gmail.com>
2024-02-23 02:13:00 -0500

    Add new rails app on top of current app

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  resources :brands do
-    resources :products
-  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
